9de0164b36d48309e991b8d0404f5dd35d424ec6
Diego Camacho <hackvan@gmail.com>
2020-09-23 18:29:09 -0500

    Add Request id to identify the request with Morgan

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const morgan = require('morgan');
+const requestId = require('express-request-id')();
 const logger = require('./config/logger');
 
 const app = express();
 
 // Setup middleware
-app.use(
-  morgan('combined', { stream: { write: (message) => logger.info(message) } })
-);
+app.use(requestId);
+app.use(logger.requests);
 
 app.get('/', (req, res, next) => {
   res.json({ message: 'Welcome to the API' });
